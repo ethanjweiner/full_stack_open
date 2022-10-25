d2db20f8a2b290d03e90cac914861cd77531b3bf
Ethan Weiner <ethanweiner20@gmail.com>
2022-10-25 11:31:59 -0400

    Exercises 1.3-1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,44 +1,49 @@
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
-
-  const parts = [
-    {
-      name: part1,
-      exercises: exercises1,
-    },
-    {
-      name: part2,
-      exercises: exercises2,
-    },
-    {
-      name: part3,
-      exercises: exercises3,
-    },
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <Total
+        total={course.parts.reduce(
+          (total, { exercises }) => total + exercises,
+          0
+        )}
+      />
     </div>
   );
 };
 
-const Header = (props) => <h1>{props.course}</h1>;
+// Components
+const Header = (props) => <h1>{props.courseName}</h1>;
 
 const Content = (props) =>
   props.parts.map(({ name, exercises }) => (
     <Part key={name} name={name} exercises={exercises} />
   ));
 
-const Part = (props) => <p>{props.name} {props.exercises}</p>
+const Part = (props) => (
+  <p>
+    {props.name} {props.exercises}
+  </p>
+);
 
 const Total = (props) => <p>Number of exercises {props.total}</p>;
 
